48ff22260c153d5ca483989a3e5ed8b082d78e2d
DaniloTenelema <daniltenelema43@gmail.com>
2022-05-26 20:32:40 -0500

    commit

---
+++ app/src/main/java/com/doodlzapp/Login.java
@@ -0,0 +1,34 @@
+package com.doodlzapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Login extends AppCompatActivity {
+
+    private Button btnIngreso;
+    private EditText editUsuario;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        editUsuario = findViewById(R.id.editTextUsuario);
+        btnIngreso = findViewById(R.id.btnIngresar);
+
+    }
+    public void onclickIniciar(View view){
+        String usuario = editUsuario.getText().toString();
+        if(usuario.equals("edisson")){
+            Intent intent = new Intent(Login.this, MainActivity.class);
+            startActivity(intent);
+        }else if(usuario.equals("danilo")){
+            Intent intent = new Intent(Login.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
